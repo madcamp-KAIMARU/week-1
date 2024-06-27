a73e60ccf2ba070d18e8c2b4e905780ace847ac1
minchurl <minchurl001@gmail.com>
2024-06-27 17:19:32 +0900

    init

--- app/src/main/java/com/example/week1/MainActivity.kt
+++ app/src/main/java/com/example/week1/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.week1.databinding.ActivityMainBinding
 
+// hello
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
