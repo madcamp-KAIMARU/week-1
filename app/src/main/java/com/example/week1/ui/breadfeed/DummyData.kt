670de8885b90c96ce5fa0592c73d73b1d03c0ccf
minchurl <minchurl001@gmail.com>
2024-06-29 15:02:59 +0900

    tab2 - Dummy Data 디스크립션 멋지게 바꿈!

--- app/src/main/java/com/example/week1/ui/breadfeed/DummyData.kt
+++ app/src/main/java/com/example/week1/ui/breadfeed/DummyData.kt
@@ -3,26 +3,26 @@ package com.example.week1.ui.breadfeed
 object DummyData {
     fun getBreadPosts(): List<BreadPost> {
         return listOf(
-            BreadPost("file:///android_asset/breads/bestseller.jpg", "Bestseller bread", "2024-07-06 12:00", 1, 4),
-            BreadPost("file:///android_asset/breads/bingsu1.jpg", "Bingsu 1", "2024-07-06 14:00", 2, 4),
-            BreadPost("file:///android_asset/breads/bingsu2.jpg", "Bingsu 2", "2024-07-06 16:00", 1, 4),
-            BreadPost("file:///android_asset/breads/buchu.jpg", "Buchu bread", "2024-07-06 18:00", 3, 4),
-            BreadPost("file:///android_asset/breads/callmard1.jpg", "Callmard 1", "2024-07-06 10:00", 1, 4),
-            BreadPost("file:///android_asset/breads/callmard2.jpg", "Callmard 2", "2024-07-06 12:00", 1, 4),
-            BreadPost("file:///android_asset/breads/fruits.jpg", "Fruits bread", "2024-07-07 14:00", 1, 4),
-            BreadPost("file:///android_asset/breads/goroke.jpg", "Goroke bread", "2024-07-07 16:00", 1, 4),
-            BreadPost("file:///android_asset/breads/hans1.jpg", "Hans 1", "2024-07-07 18:00", 1, 4),
-            BreadPost("file:///android_asset/breads/ts.jpg", "Ts bread", "2024-07-07 10:00", 1, 4),
-            BreadPost("file:///android_asset/breads/long.jpg", "Long bread", "2024-07-07 12:00", 1, 4),
-            BreadPost("file:///android_asset/breads/mango1.jpg", "Mango 1", "2024-07-07 14:00", 1, 4),
-            BreadPost("file:///android_asset/breads/mango2.jpg", "Mango 2", "2024-07-07 16:00", 1, 4),
-            BreadPost("file:///android_asset/breads/mango3.jpg", "Mango 3", "2024-07-07 18:00", 1, 4),
-            BreadPost("file:///android_asset/breads/manybread.jpg", "Many bread", "2024-07-07 10:00", 1, 4),
-            BreadPost("file:///android_asset/breads/manycake.jpg", "Many cake", "2024-07-07 12:00", 1, 4),
-            BreadPost("file:///android_asset/breads/meari.jpg", "Meari bread", "2024-07-07 14:00", 1, 4),
-            BreadPost("file:///android_asset/breads/noodle.jpg", "Noodle bread", "2024-07-07 16:00", 1, 4),
-            BreadPost("file:///android_asset/breads/salt.jpg", "Salt bread", "2024-07-07 18:00", 1, 4),
-            BreadPost("file:///android_asset/breads/sunsu.jpg", "Sunsu bread", "2024-07-07 10:00", 1, 4)
+            BreadPost("file:///android_asset/breads/bestseller.jpg", "성심당 베스트셀러 다 털러 가자구! 카이택시에서 만나서 본점으로 고고🚖🥐", "2024-07-06 12:00", 1, 4),
+            BreadPost("file:///android_asset/breads/bingsu1.jpg", "포장빙수 맛집이래~ 같이 먹으러 가자! 택승에서 만나서 택시 타자🍧🚕", "2024-07-06 14:00", 2, 4),
+            BreadPost("file:///android_asset/breads/bingsu2.jpg", "빙수 같이 먹고 더위 날리자❄️🍨", "2024-07-06 16:00", 1, 4),
+            BreadPost("file:///android_asset/breads/buchu.jpg", "부추빵 진짜 맛있어! DCC점에서 만나자~🥖🚍", "2024-07-06 18:00", 3, 4),
+            BreadPost("file:///android_asset/breads/callmard1.jpg", "어은동 콜마르브레드 가볼까? 가까워서 금방 감🍞👫", "2024-07-06 10:00", 1, 4),
+            BreadPost("file:///android_asset/breads/callmard2.jpg", "끼리바나나 꼭 먹어봐야해~ 콜마르브레드로 고고🍌🥖", "2024-07-06 12:00", 1, 4),
+            BreadPost("file:///android_asset/breads/fruits.jpg", "과일시루 먹으러 롯백 성심당 가자! 🍰🚖", "2024-07-07 14:00", 1, 4),
+            BreadPost("file:///android_asset/breads/goroke.jpg", "고로케 맛집 DCC점! 버스타고 같이가자🚌🥐", "2024-07-07 16:00", 1, 4),
+            BreadPost("file:///android_asset/breads/hans1.jpg", "크로와상 맛집 한스브레드~ 같이가서 먹자🥐😋", "2024-07-07 18:00", 1, 4),
+            BreadPost("file:///android_asset/breads/ts.jpg", "성심당하면 튀소지! 본점으로 튀소 먹으러 고고🍞✨", "2024-07-07 10:00", 1, 4),
+            BreadPost("file:///android_asset/breads/long.jpg", "먹물방망이 맛있대! 본점으로 출발🥖😄", "2024-07-07 12:00", 1, 4),
+            BreadPost("file:///android_asset/breads/mango1.jpg", "망고시루 DCC점에서 9시 레이드! 🍰🍹", "2024-07-07 07:40", 1, 4),
+            BreadPost("file:///android_asset/breads/mango2.jpg", "롯백점 망고시루 8시에 사러가자! 🍹🕗", "2024-07-07 07:20", 1, 4),
+            BreadPost("file:///android_asset/breads/mango3.jpg", "성심당 본점 망고시루 같이 먹으러 고고🕕🥭", "2024-07-07 06:50", 1, 4),
+            BreadPost("file:///android_asset/breads/manybread.jpg", "오늘 여기 있는 빵 다 먹어보자! 본점으로 가자🍞🍰", "2024-07-07 10:00", 1, 4),
+            BreadPost("file:///android_asset/breads/manycake.jpg", "성심당 케이크부띠끄 다 해치우자🍰🎂", "2024-07-07 12:00", 1, 4),
+            BreadPost("file:///android_asset/breads/meari.jpg", "성심당 메아리 먹으러 가자~ 🍞🍃", "2024-07-07 14:00", 1, 4),
+            BreadPost("file:///android_asset/breads/noodle.jpg", "성심당 우동 맛집이라던데... 🍜😋", "2024-07-07 16:00", 1, 4),
+            BreadPost("file:///android_asset/breads/salt.jpg", "소금빵 본점에서 먹자~ 🧂🍞", "2024-07-07 18:00", 1, 4),
+            BreadPost("file:///android_asset/breads/sunsu.jpg", "성심당 순수롤 같이 먹으러 가자! 🌞🍰", "2024-07-07 10:00", 1, 4)
         )
     }
 }
