2e3dce50c822c94de1aaeb49fbe616ff33ba3ef7
minchurl <minchurl001@gmail.com>
2024-06-29 20:11:22 +0900

    메인 액션바 추가!

--- app/src/main/java/com/example/week1/MainActivity.kt
+++ app/src/main/java/com/example/week1/MainActivity.kt
@@ -20,6 +20,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Hide the ActionBar
+        supportActionBar?.hide()
+
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
